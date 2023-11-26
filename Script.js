389d9bbbac259af434f704bdb0062ff472879476
Aditya <118987965+Aadistudios21@users.noreply.github.com>
2023-11-26 13:22:49 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,13 @@
+// script.js
+
+// Function to toggle a CSS class for animation
+function toggleAnimation() {
+  const animatedSection = document.querySelector('.animated-section');
+  animatedSection.classList.toggle('animate');
+}
+
+// Add event listener to trigger animation on page load
+window.addEventListener('load', toggleAnimation);
+
+// You can add more event listeners or functions for additional interactions
+￼Enter
